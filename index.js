6ff29c1581aed60e440a071dd6ff314ce2eeb98a
Mukund Chaturvedi <124137083+mukund2988@users.noreply.github.com>
2024-07-09 21:06:44 +0530

    CRUD opertation

---
+++ index.js
@@ -0,0 +1,7 @@
+const fs =require("fs")
+ //fs.mkdirSync("CRUD_thapa")
+//fs.writeFileSync('CRUD_thapa/bio.text',"this is for crud n opr")
+// const data = fs.readFileSync("CRUD_thapa/bio.txt","utf8");
+// console.log(data);
+
+// fs.renameSync("CRUD_thapa/bio.text", "CRUD_thapa/mybio.text")
